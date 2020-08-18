74270bb0e6e3fadf53f9354eaca4fd14322be8a6
hsalas006 <hsalas006@github.com>
2020-08-18 16:51:40 -0600

    get and show multiple users

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    const resp = await axios.get("https://api.github.com/users");
+
+    this.setState({ users: resp.data, loading: false });
+
+    console.log(this.state.users);
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
